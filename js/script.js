95a9307da26b7621ae27d963ba0d5243623387a6
Analch <anaele.louchez@my-digital-school.org>
2025-03-02 15:42:07 +0100

    🚀 deploy convertisseur de devises

--- js/script.js
+++ js/script.js
@@ -1,30 +1,39 @@
-function calcul(valeurEntre){
-    let devise_source = document.getElementById("devise-source");
-    let devise_cible = document.getElementById("devise-cible");
-    console.log(`Devise source : ${devise_source.value}`);
-    console.log(`Devise cible : ${devise_cible.value}`);
+//taux de change :
+const converterValues = [
+    {name: 'EUR', value: 0.95, symbol: '€'},
+    {name: 'USD', value: 1.00, symbol: '$'},
+    {name: 'GBP', value: 0.79, symbol: '£'},
+    {name: 'JPY', value: 151.44, symbol: '¥'},
+]
 
-    const conversion = {
-        "EUR": {"EUR": 1, "USD": 1.09, "GBP": 0.85, "JPY": 158.42},
-        "USD": {"EUR": 0.92, "USD": 1, "GBP": 0.78, "JPY": 145.90},
-        "GBP": {"EUR": 1.17, "USD": 1.28, "GBP": 1, "JPY": 186.73},
-        "JPY": {"EUR": 0.0063, "USD": 0.0069, "GBP": 0.0054, "JPY": 1}
-    }
-    let output =  ;
-    let result = valeurEntre * conversion[devise_source.value][devise_cible.value];
-    if (result === 0){
-        output.textContent = `Résultat : 0 ${devise_cible.value}`;
-    } else {
-        output.textContent = `Résultat : ${result} ${devise_cible.value}`;
-    }
+//convertisseur de devises :
+const converterForm = document.getElementById('currenciesConverter');
+const currencyIn = document.getElementById("currencyIn");
+const currencyOut = document.getElementById("currencyOut");
+const inputInValue = document.getElementById("valueIn");
+const inputOutValue = document.getElementById("valueOut");
+
+function convertCurrencies(value, changeRateIn, changeRateOut) {
+    return (value * changeRateOut) / changeRateIn;
 }
 
-const buttonCalculateConversion = document.getElementById('');
-const inputInValue = document.getElementById('');
+function getChangeRate(changeName) {
+    return converterValues.find((value) => changeName === value.name);
+}
 
-buttonCalculateConversion.addEventListener("click", () => {
-    let valeurEntre = parseFloat(inputDeviceInfo.value);
-    console.log(valeurEntre);
-    calcul(valeurEntre);
+//swap de devise :
+function swapCurrencies() {
+    [currencyIn.value, currencyOut.value] = [currencyOut.value, currencyIn.value]
+}
+
+converterForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const valueIn = parseFloat(inputInValue.value);
+    const changeRateValueIn = getChangeRate(currencyIn.value)
+    const changeRateValueOut = getChangeRate(currencyOut.value);
+    const conversionResult = convertCurrencies(valueIn, changeRateValueIn.value, changeRateValueOut.value)
+
+    inputOutValue.textContent = `${conversionResult.toFixed(2)} ${currencyOut.value}`;
+
+
 });
-
